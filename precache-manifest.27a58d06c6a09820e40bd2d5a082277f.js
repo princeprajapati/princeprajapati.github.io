91f5b4eeef24ef474ba9f77e7b715cded0078361
Prince Prajapati <pprajapati2316@conestogac.on.ca>
2023-06-18 13:35:42 -0400

    Updates

---
+++ precache-manifest.27a58d06c6a09820e40bd2d5a082277f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d8a015f7f494839cc3e55c727ea2a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927e52e7c6bb380853fd",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2be7458029cd2ef3f886",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "927e52e7c6bb380853fd",
+    "url": "/static/js/2.da0ad8ad.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.da0ad8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be7458029cd2ef3f886",
+    "url": "/static/js/main.4304eb27.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
